8f1c8d625ff6e3474dcbd723eb4f05394aeac915
kingjeongkong <wjdqls9223@gmail.com>
2024-12-03 16:14:56 +0900

    전체 프로젝트 세팅

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,6 +3,9 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors(); // CORS 활성화
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
