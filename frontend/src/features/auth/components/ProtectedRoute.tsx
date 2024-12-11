bbadfb03da38555adcc0cf25b46ba841c8469f5f
kingjeongkong <wjdqls9223@gmail.com>
2024-12-12 00:11:06 +0900

    [Feat/#3] ProtectedRoute 컴포넌트 구현

---
+++ frontend/src/features/auth/components/ProtectedRoute.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import useAuth from '../hooks/useAuth';
+import { Navigate } from 'react-router-dom';
+
+interface ProtectedRouteProps {
+  children: React.ReactNode;
+}
+
+const ProtectedRoute = ({ children }: ProtectedRouteProps) => {
+  const { isAuthenticated, loading } = useAuth();
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (!isAuthenticated) {
+    return <Navigate to={'/'} />;
+  }
+
+  return <>{children}</>;
+};
+
+export default ProtectedRoute;
