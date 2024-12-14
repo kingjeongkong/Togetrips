0ccf1b705adf54ea3ef21b6685b46fbc417c2973
kingjeongkong <wjdqls9223@gmail.com>
2024-12-15 03:23:27 +0900

    [Feat/#9] UserProfile type 정의 및 signUp시 데이터 수정

---
+++ frontend/src/features/Main/types/profileTypes.ts
@@ -0,0 +1,10 @@
+export interface UserProfile {
+  uid: string;
+  name: string;
+  email: string;
+  photoURL?: string;
+  tags: string;
+  bio: string;
+  createdAt: string;
+  updatedAt: string;
+}
