1a4ec7f7aba29eea8636bbed5e8ef80e2aa515bf
kingjeongkong <wjdqls9223@gmail.com>
2025-01-10 15:53:39 +0900

    [Feat/#30] 메시지 보낸 시간 메시지 박스 밖으로 레이아웃 재조정

--- frontend/src/features/Main/section/Chat/components/ChatRoomMessage.tsx
+++ frontend/src/features/Main/section/Chat/components/ChatRoomMessage.tsx
@@ -1,4 +1,4 @@
-import { formatMessageTime } from "../../../../../utils/dateUtils";
+import { formatMessageTime } from '../../../../../utils/dateUtils';
 
 interface ChatRoomMessageProps {
   text: string;
@@ -8,20 +8,16 @@ interface ChatRoomMessageProps {
 
 const ChatRoomMessage = ({ text, time, isMine }: ChatRoomMessageProps) => {
   return (
-    <div className={`flex ${isMine ? 'justify-end' : 'justify-start'}`}>
+    <div className={`flex gap-1 ${isMine ? 'justify-end' : 'justify-start'}`}>
+      <div className={`text-xs flex items-end pb-1 ${isMine ? 'order-1' : 'order-2'}`}>
+        {formatMessageTime(time)}
+      </div>
       <div
         className={`max-w-[80%] rounded-2xl px-4 py-2 ${
-          isMine ? 'bg-blue-500 text-white' : 'bg-white'
+          isMine ? 'bg-blue-500 text-white order-2' : 'bg-white order-1'
         }`}
       >
         <p className="break-words">{text}</p>
-        <p
-          className={`text-xs mt-1 text-right ${
-            isMine ? 'text-blue-100' : 'text-gray-500'
-          }`}
-        >
-          {formatMessageTime(time)}
-        </p>
       </div>
     </div>
   );
