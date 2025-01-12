95c7f3d64053dd117e45c3a3605bcbe01ea50db7
kingjeongkong <wjdqls9223@gmail.com>
2025-01-12 12:42:04 +0900

    수정

--- frontend/src/pages/Main/ChatLayout.tsx
+++ frontend/src/pages/Main/ChatLayout.tsx
@@ -11,9 +11,9 @@ const ChatLayout = () => {
   if (isMobile) {
     // ------ (A) 모바일 레이아웃 ------
     return (
-      <div className={`flex overflow-hidden h-[--webkit-fill-available] bg-gray-100 pb-16 md:pb-0 ${isRootPath ? 'h-screen' : 'max-h-screen'}`}>
+      <div className="flex flex-col h-screen bg-gray-100">
         <Sidebar />
-        <main className="flex-1">
+        <main className="flex-1 overflow-hidden">
           {/* 모바일에서는 <Outlet /> 안에서 index이면 ChatList, :chatId이면 ChatRoom */}
           <Outlet />
         </main>
