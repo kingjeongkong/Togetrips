aa618ddcc8b7a5fa2f1db99781eec74600c2ebd6
kingjeongkong <wjdqls9223@gmail.com>
2025-01-13 22:26:54 +0900

    [Fix] 화면 사이즈 조정시 Profile 스크롤 안됨 이슈 해결

--- frontend/src/pages/Main/Profile.tsx
+++ frontend/src/pages/Main/Profile.tsx
@@ -3,9 +3,9 @@ import MainProfile from '../../features/Main/section/Profile/components/MainProf
 
 const Profile = () => {
   return (
-    <div className="flex min-h-screen bg-gray-100">
+    <div className="flex min-h-screen bg-gray-100 overflow-y-auto">
       <Sidebar />
-      <main className="flex-1 pt-16 md:pt-5 md:pl-60">
+      <main className="flex-1 pt-16 pb-20 md:pt-5 md:pl-60 md:pb-5">
         <MainProfile />
       </main>
     </div>
