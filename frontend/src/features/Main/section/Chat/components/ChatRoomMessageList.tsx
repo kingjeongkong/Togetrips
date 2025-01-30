62079fa34904060243706326be9ac5236c47ec13
kingjeongkong <wjdqls9223@gmail.com>
2025-01-30 11:53:06 +0900

    [Feat/#37] ChatRoom 스크롤 하단 이동 버튼 구현

--- frontend/src/features/Main/section/Chat/components/ChatRoomMessageList.tsx
+++ frontend/src/features/Main/section/Chat/components/ChatRoomMessageList.tsx
@@ -1,8 +1,9 @@
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { formatMessageDate } from '../../../../../utils/dateUtils';
 import { Message } from '../types/chatTypes';
 import ChatRoomDateDivider from './ChatRoomDateDivider';
 import ChatRoomMessage from './ChatRoomMessage';
+import { BsArrowDownCircleFill } from 'react-icons/bs';
 
 interface ChatRoomMessageListProps {
   messages: Message[];
@@ -11,6 +12,7 @@ interface ChatRoomMessageListProps {
 
 const ChatRoomMessageList = ({ messages, currentUserID }: ChatRoomMessageListProps) => {
   const containerRef = useRef<HTMLDivElement>(null);
+  const [showScrollButton, setShowScrollButton] = useState(false);
 
   const messagesByDate = messages.reduce(
     (groups: { [key: string]: Message[] }, message) => {
@@ -24,17 +26,37 @@ const ChatRoomMessageList = ({ messages, currentUserID }: ChatRoomMessageListPro
     {}
   );
 
-  useEffect(() => {
-    // Scroll to the bottom
+  const scrollToBottom = () => {
     if (containerRef.current) {
       containerRef.current.scrollTop = containerRef.current.scrollHeight;
     }
+  };
+
+  const handleScroll = () => {
+    if (containerRef.current) {
+      const { scrollTop, scrollHeight, clientHeight } = containerRef.current;
+      const isNearBottom = scrollHeight - scrollTop - clientHeight < 300;
+      setShowScrollButton(!isNearBottom);
+    }
+  };
+
+  useEffect(() => {
+    const container = containerRef.current;
+    if (container) {
+      container.addEventListener('scroll', handleScroll);
+
+      return () => container.removeEventListener('scroll', handleScroll);
+    }
+  }, []);
+
+  useEffect(() => {
+    scrollToBottom();
   }, [messages]);
 
   return (
     <div
       ref={containerRef}
-      className="flex-1 overflow-y-auto px-2 py-2 bg-gray-200"
+      className="relative flex-1 overflow-y-auto px-2 py-2 bg-gray-200"
       style={{ height: '100%', maxHeight: '100%' }}
     >
       {Object.entries(messagesByDate).map(([date, messages]) => (
@@ -50,6 +72,14 @@ const ChatRoomMessageList = ({ messages, currentUserID }: ChatRoomMessageListPro
           ))}
         </div>
       ))}
+      {showScrollButton && (
+        <button
+          onClick={scrollToBottom}
+          className="fixed right-4 bottom-36 md:bottom-20 transition-all text-white"
+        >
+          <BsArrowDownCircleFill className="w-8 h-8 md:w-10 md:h-10" />
+        </button>
+      )}
     </div>
   );
 };
