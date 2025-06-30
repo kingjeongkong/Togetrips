e943f580b2973b1dedc8fb2f023f3ccbeaeac22b
kingjeongkong <wjdqls9223@gmail.com>
2025-06-30 18:06:29 +0900

    [fix] 빌드 에러 수정

--- nextjs/src/components/ProtectedRoute.tsx
+++ nextjs/src/components/ProtectedRoute.tsx
@@ -10,7 +10,7 @@ interface ProtectedRouteProps {
 }
 
 export default function ProtectedRoute({ children }: ProtectedRouteProps) {
-  const { data: session, status } = useSession();
+  const { status } = useSession();
   const router = useRouter();
 
   useEffect(() => {
