b33b9247f1a310c52cbb174c677c7811a6fad1a2
kingjeongkong <wjdqls9223@gmail.com>
2025-07-24 16:00:55 +0900

    [fix] 구글 맵 api 스크립트 중복 호출 제거

--- nextjs/src/app/(main)/layout.tsx
+++ nextjs/src/app/(main)/layout.tsx
@@ -1,17 +1,14 @@
 'use client';
 
 import Sidebar from '@/features/shared/components/Sidebar';
-import { LoadScript } from '@react-google-maps/api';
 
 export default function MainLayout({ children }: { children: React.ReactNode }) {
   return (
-    <LoadScript googleMapsApiKey={process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY!}>
-      <div className="flex h-screen overflow-hidden bg-gray-100">
-        <Sidebar />
-        <main className="flex-1 md:pl-60">
-          <div className="h-full pt-16 md:pt-0 overflow-y-auto">{children}</div>
-        </main>
-      </div>
-    </LoadScript>
+    <div className="flex h-screen overflow-hidden bg-gray-100">
+      <Sidebar />
+      <main className="flex-1 md:pl-60">
+        <div className="h-full pt-16 md:pt-0 overflow-y-auto">{children}</div>
+      </main>
+    </div>
   );
 }
