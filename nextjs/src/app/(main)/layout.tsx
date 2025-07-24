f1fb370febe081532316b0744608755396aa48af
왕정빈 <109719591+kingjeongkong@users.noreply.github.com>
2025-07-24 17:39:31 +0900

    [fix] 구글 맵 스크립트 중복 호출 수정 (#77)

--- nextjs/src/app/(main)/layout.tsx
+++ nextjs/src/app/(main)/layout.tsx
@@ -1,11 +1,13 @@
 'use client';
 
+import { MapInitializer } from '@/components/MapInitializer';
 import Sidebar from '@/features/shared/components/Sidebar';
 import { LoadScript } from '@react-google-maps/api';
 
 export default function MainLayout({ children }: { children: React.ReactNode }) {
   return (
     <LoadScript googleMapsApiKey={process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY!}>
+      <MapInitializer />
       <div className="flex h-screen overflow-hidden bg-gray-100">
         <Sidebar />
         <main className="flex-1 md:pl-60">
