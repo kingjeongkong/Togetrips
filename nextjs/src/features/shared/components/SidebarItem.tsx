ca8df2abea3571e8aa226161bd110c17b53e1a3f
kingjeongkong <wjdqls9223@gmail.com>
2025-08-07 01:22:03 +0900

    [chore] sidebar 디자인 수정

--- nextjs/src/features/shared/components/SidebarItem.tsx
+++ nextjs/src/features/shared/components/SidebarItem.tsx
@@ -18,7 +18,7 @@ const SidebarItem = ({ title, icon: Icon, to, count }: SidebarItemProps) => {
   return (
     <Link
       href={to}
-      className={`flex flex-row items-center justify-center px-2 py-4 rounded w-11/12 relative ${
+      className={`flex flex-row items-center justify-center px-2 py-4 rounded w-11/12 relative md:justify-start md:px-6 md:py-4 md:gap-2 ${
         isActive ? 'bg-sky-700 text-white' : 'text-gray-600 hover:bg-sky-600 hover:text-white'
       }`}
       aria-label={`Navigate to ${title.toLowerCase()}`}
@@ -27,7 +27,7 @@ const SidebarItem = ({ title, icon: Icon, to, count }: SidebarItemProps) => {
       <span className="hidden md:inline">{title}</span>
 
       {count && count > 0 && (
-        <div className="absolute top-1/6 right-5/12 md:top-1/6 md:right-1/4">
+        <div className="absolute top-1/6 right-5/12 md:left-1/7">
           <span className="flex items-center justify-center w-4 h-4 md:w-5 md:h-5 text-xs font-bold text-white bg-red-500 rounded-full border-1 border-white md:border-none">
             {count > 99 ? '99+' : count}
           </span>
